0d82d3a1d12437945edc84ac9c09d7c0f153da4b
pundalikkore <138847504+pundalikkore@users.noreply.github.com>
2026-03-25 07:29:44 +0530

    Fix: replace nodemon with node for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 // index.js (server entry)
-import cors from 'cors';
-import 'dotenv/config';
-import express from 'express';
-import { connectDB } from './config/db.js';
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import { connectDB } from "./config/db.js";
 
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
-import userRouter from './routes/userRoute.js';
-import bookRouter from './routes/bookRoute.js';
-import orderRouter from './routes/orderRoute.js';
-import cartRouter from './routes/cartRoute.js';
+import userRouter from "./routes/userRoute.js";
+import bookRouter from "./routes/bookRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,17 +19,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors({
-  origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:5177', 'http://localhost:5178'];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = ["http://localhost:5175", "http://localhost:5176"];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -38,14 +40,14 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 // Routes
-app.use('/api/user', userRouter);
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/api/book', bookRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
+app.use("/api/user", userRouter);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/book", bookRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
-app.get('/', (req, res) => {
-  res.send('API Working');
+app.get("/", (req, res) => {
+  res.send("API Working");
 });
 
 app.listen(port, () => {
